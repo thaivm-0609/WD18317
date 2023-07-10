1904a2a27cca5215600e1a2187a8b42b7e2e68b4
lovent <vuongthai95@gmail.com>
2023-07-10 15:51:08 +0700

    init project

---
+++ test.php
@@ -0,0 +1,82 @@
+<?php
+
+// $a = true;
+// $b = '1';
+// $b = 2;
+// $c = ++$b;
+// $c = $a <=> $b;
+// echo gettype($b);
+// echo gettype((int)$b);
+// echo gettype((boolean)$b);
+// echo $a;
+// echo $d;
+
+    // $users = [1,2,3,4,5];
+    // $users = array(1,2,3,4,5);
+
+    // $users[0] = 1;
+    // $users[1] = 2;
+    // $users[2] = 3;
+    // $users[3] = 4;
+    // $users[4] = 5;
+
+    // $users[] = 10;
+
+    // $a = [
+    //     [1,2,3],
+    //     [4,5,6],
+    //     [7,8,9],
+    //     [10,11,12],
+    //     [13,14,15],
+    // ];
+    //a 
+
+    // const VAT = 0.1;
+    // define('VAT', 0.1, TRUE);
+    // echo VAT;
+    
+    // foreach ($a as $item)
+    // {
+    //     foreach ($item as $x) 
+    //     {        
+    //         $y = $x*VAT;
+
+    //         echo $y. "\n";
+    //     }
+    // }
+    
+    // if ($a >= 0) {
+    //     echo 'a la so duong';
+    // } else {
+    //     echo 'a la so am';
+    // }
+
+    // $checkSoDuong = ($a >= 0) ? true : false;
+    // echo (int)$checkSoDuong;
+    // $a = 0;
+    // switch ($a) {
+    //     case 1: 
+    //         echo 'a la so duong';
+    //         break;
+    //     case 2: 
+    //         echo 'a bang 0';
+    //         break;
+    //     case 3: 
+    //         echo 'a la so am';
+    //         break;
+    //     default: echo 'day la default';
+    // }
+        // $a = 0;
+        // $b = match($a) {
+        //     1 => 'True',
+        //     2 => 'False',
+        //     // default => 'default'
+        // };
+        // echo $b;
+
+        // switch($a) {
+        //     case 1: echo 'True'; break;
+        //     case 2: echo 'False'; break;
+        //     default: echo 'default';
+        // };
+?>
