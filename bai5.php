a226b58737c79d9a35816f4ee252769bf4cb567a
lovent <vuongthai95@gmail.com>
2023-07-14 10:32:27 +0700

    function buoi 5

---
+++ bai5.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+    function tinhHieu($soBe, $soLon = 3, $soVua = 7) {
+        echo $soLon - $soBe - $soVua;
+    }
+    tinhHieu(22);
+    // tinhHieu(22,5);
+
+    // echo str_repeat('abc', 10);
+    // echo strtoupper('string viet thuong');
+?>
