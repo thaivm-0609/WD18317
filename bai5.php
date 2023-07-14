e26005b161a69cfaef728c89222463a326369766
lovent <vuongthai95@gmail.com>
2023-07-14 15:24:38 +0700

    ham an danh

--- bai5.php
+++ bai5.php
@@ -1,13 +1,40 @@
 <?php
 
-
-
-    function tinhHieu($soBe, $soLon = 3, $soVua = 7) {
-        echo $soLon - $soBe - $soVua;
+    function tinhHieu($soBe, ...$params) {
+        print_r($params);
     }
-    tinhHieu(22);
+    // tinhHieu(10, 11, 15);
     // tinhHieu(22,5);
 
     // echo str_repeat('abc', 10);
     // echo strtoupper('string viet thuong');
+
+    function say($name) {
+        echo "Hello $name";
+    }
+
+    // say('thaivm2');
+
+    //ham an danh
+    $a = function($name) 
+    {
+        echo "Hello $name";
+    };
+
+    // $a('thaivm2');
+
+    //tham so bat dinh
+    function tinhTong($soBe, ...$params) {
+        $soParam = func_num_args();
+        $args = func_get_args();
+
+        $tong = 0;
+        foreach ($params as $number) {
+            $tong += $number;
+        }
+
+        echo $tong;
+    };
+
+    tinhTong(10, 11,12,13,14,15);
 ?>
